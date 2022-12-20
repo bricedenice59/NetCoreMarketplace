7dca6fefe0c672313612ad2fe27704fdcd9160ab
Brice Grenard <brice.grenard@gmail.com>
2022-12-20 10:23:55 +0800

    Add CORS policy for test purpose

--- API/Program.cs
+++ API/Program.cs
@@ -6,6 +6,8 @@ using Infrastructure.Data;
 using Infrastructure.SeedData;
 using Microsoft.EntityFrameworkCore;
 
+const string corsPolicy = "CorsPolicy";
+
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
@@ -20,6 +22,10 @@ builder.Services.AddSwaggerGen();
 // Add Application Db Context options
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options => 
+    options.AddPolicy(corsPolicy, 
+        policyBuilder => policyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:7120")));
 
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
@@ -51,6 +57,8 @@ if (app.Environment.IsDevelopment())
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
 app.UseHttpsRedirection();
+
+app.UseCors(corsPolicy);
 
 app.UseAuthorization();
 
