fa0f094b25aedaa394cd8e62959b3ca8c1e47e96
Brice Grenard <brice.grenard@gmail.com>
2022-12-26 17:15:27 +0800

    Add files, modules,service... for the rendering of the shop products. Add a very basic UI for the shop component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CoreModule,
+    ShopModule,
+  ],
 })
 export class AppModule {}
