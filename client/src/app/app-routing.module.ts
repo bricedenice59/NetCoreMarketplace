f086a2277dfa9163a042a1ce5d45ab414a0965a5
Brice Grenard <brice.grenard@gmail.com>
2023-01-06 19:10:38 +0800

    Add some basic routing rules and empty components for the next dev task (page: single product details)

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsDetailsComponent } from './shop/products-details/products-details.component';
+import { ShopComponent } from './shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'shop/:id', component: ProductsDetailsComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -4,10 +4,18 @@ import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { PaginationComponent } from '../shared/components/pagination/pagination.component';
 import { ShopPagingHeaderComponent } from './shop-paging-header/shop-paging-header.component';
+import { ProductsDetailsComponent } from './products-details/products-details.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent, PaginationComponent, ShopPagingHeaderComponent],
-  imports: [CommonModule],
+  declarations: [
+    ShopComponent,
+    ProductItemComponent,
+    PaginationComponent,
+    ShopPagingHeaderComponent,
+    ProductsDetailsComponent,
+  ],
+  imports: [CommonModule, RouterModule],
   exports: [ShopComponent],
 })
 export class ShopModule {}
